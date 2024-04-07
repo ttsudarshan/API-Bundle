bcb354d127410356eaa88f104a7c0e478741bd91
abhishek-pokhrel <abhishekpokhrel53@gmail.com>
2024-04-07 01:19:11 -0400

    Added pokemon api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const home = require("./routes/home");
 const quotes = require("./routes/quotes");
 const horoscopes = require("./routes/horoscopes");
 const today = require("./routes/today");
+const pokemon = require("./routes/pokemon");
 const cors = require('cors');
 
 
@@ -18,6 +19,7 @@ app.use("/", home);
 app.use("/quotes/", quotes);
 app.use("/horoscopes", horoscopes);
 app.use("/today", today);
+app.use("/pokemon", pokemon);
 
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
